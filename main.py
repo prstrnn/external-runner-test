e6c92561ab2621de7b578e768b3d172a0add95bf
Perestoronin, Andrey <andrey.perestoronin@intel.com>
2025-05-19 09:35:56 +0100

    Add script

---
+++ main.py
@@ -0,0 +1,13 @@
+def draw_duck():
+    duck = """
+       _      _
+     >(.)__ <(.)__
+      (___/  (___/ 
+    """
+    return duck
+
+def main():
+    print(draw_duck())
+
+if __name__ == "__main__":
+    main()
